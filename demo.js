eae546ad7d6547e9c8ab7103c51100a6a13a2f98
HONEYANASHATH <91932860+HONEYANASHATH@users.noreply.github.com>
2023-11-25 21:13:23 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,88 @@
+/*function samsung()
+{
+console.log("sam")
+}
+function oppo()
+{
+console.log("op")
+}
+function iphone()
+{
+console.log("ip")
+}
+samsung()
+oppo()
+iphone()*/
+
+/*var a=10
+var b=20
+function add()
+{
+    console.log(a+b)
+}
+add() */
+
+/*var factor="vijay"
+var fplayer="dhoni"
+var fmovie="ps2"
+function fav()
+{
+    console.log('favo actpr:'+factor)
+    console.log('favo movie:'+fmovie)
+    console.log('favo play:'+fplayer)
+}
+
+fav()*/
+
+/*function area(length,breadth)
+{
+   console.log("areais"+length*breadth) 
+}
+area(10,10)*/
+ 
+/*
+function add(a,b)
+{
+ return(a+b)
+}
+var total=add(10 ,10)
+console.log(total)
+*/
+
+/*var homework=true
+if(homework)
+{
+    console.log("greate job")
+}
+else
+{
+    console.log("finish before playingclear")
+}
+*/
+/*var year= "autumn"
+if(year=="spring")
+{
+    console.log("enjoy the blooming")
+}
+
+if(year="summer")
+{
+    console.log("have fun in sun")
+}
+
+if(year=="autumn")
+{
+le.log("colorful leaves")
+}
+
+if(year=="winter")
+{
+    console.log("stay warm")
+} */
+
+/*for(count=1;count<=10;count=count+1)
+    {
+        console.log(count+"*2="+1)
+    }*/
+
+    
